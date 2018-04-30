50b019fa1ee49cf2f6c9c4b479b415cf615e0096
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-04-30 17:15:35 +0200

    Moar debug

--- syceron.py
+++ syceron.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 from xml.dom.pulldom import START_ELEMENT, CHARACTERS, END_ELEMENT, parse
 from xml.dom.minidom import Element, Text
 
 doc = parse(sys.argv[1])
+debug = len(sys.argv) == 3 and sys.argv[2] == "--debug"
+debug_more = len(sys.argv) == 3 and sys.argv[2] == "--debug-more"
 indent_level = 0
 visited = []
 
 is_syceron = False
 
 accepted_seance_context = [
-  "CompteRendu|Metadonnees|DateSeance",
-  "CompteRendu|Metadonnees|Sommaire|Sommaire1|TitreStruct|Intitule",
-  "CompteRendu|Contenu|Quantiemes|Journee",
-  "CompteRendu|Contenu|ouverture_seance|paragraphe|ORATEURS|ORATEUR|NOM",
-  "CompteRendu|Contenu|ouverture_seance|paragraphe|texte",
+  re.compile("CompteRendu@Metadonnees@DateSeance"),
+  re.compile("CompteRendu@Metadonnees@Sommaire@Sommaire1@TitreStruct@Intitule"),
+  re.compile("CompteRendu@Contenu@Quantiemes@Journee"),
+  re.compile("CompteRendu@Contenu@ouverture_seance@paragraphe@ORATEURS@ORATEUR@NOM"),
+  re.compile(".*@paragraphe@texte.*"),
 ]
 seance_context = None
 
@@ -39,14 +42,21 @@ for event, node in doc:
         print(seance_context)
         seance_context = {}
 
-  if type(node) == Text:
-    #print("/".join(visited))
-    #print(" "*indent_level, str(type(node)), ":", node.toxml())
-    visitedFullPath = "|".join(visited)
-    #print(visitedFullPath, ":", node.nodeValue)
+  if debug:
+    print("DEBUG:", "/".join(visited), visited)
+    print("DEBUG:", " "*indent_level, str(type(node)), ":", node.toxml())
 
-    if visitedFullPath in accepted_seance_context:
-      seance_context[visited[-1:][0]] = node.nodeValue
+  if type(node) == Text:
+    visitedFullPath = "@".join(visited)
+
+    if debug_more:
+        print("DEBUG:", "visitedFullPath=" + visitedFullPath, ":", node.nodeValue, )
+
+    if any(regex.match(visitedFullPath) for regex in accepted_seance_context):
+      try:
+        seance_context[visited[-1:][0]] += " " + node.nodeValue
+      except KeyError:
+        seance_context[visited[-1:][0]] = node.nodeValue
 
   if event == END_ELEMENT:
     indent_level -= 2
