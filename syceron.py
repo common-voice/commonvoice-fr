79aa878022b1d16866fa2ffb43d8831648a713a5
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-05-02 19:20:36 +0200

    Hacking some output files

--- syceron.py
+++ syceron.py
@@ -2,9 +2,11 @@
 
 import sys
 import re
+import os
 import argparse
 import networkx as nx
 import networkx.drawing.nx_pydot as nx_pydot
+import json
 
 from xml.dom.pulldom import START_ELEMENT, CHARACTERS, END_ELEMENT, parse
 from xml.dom.minidom import Element, Text
@@ -17,6 +19,7 @@ parser.add_argument('--print-tree', action='store_true', help='Only print XML tr
 parser.add_argument('--tree-output', type=argparse.FileType('w'), help='Where to store XML tree structure. Use \'-\' for stdout.')
 
 parser.add_argument('file', type=str, help='Source XML file')
+parser.add_argument('output', type=str, help='Output directory')
 
 args = parser.parse_args()
 
@@ -33,9 +36,19 @@ accepted_seance_context = [
   re.compile("CompteRendu@Metadonnees@Sommaire@Sommaire1@TitreStruct@Intitule"),
   re.compile("CompteRendu@Contenu@Quantiemes@Journee"),
   #re.compile("CompteRendu@Contenu@ouverture_seance@paragraphe@ORATEURS@ORATEUR@NOM"),
-  re.compile(".*@paragraphe@texte.*"),
+  re.compile(".*@paragraphe@texte$"),
 ]
 seance_context = None
+
+chars_mapping = {
+  'o': 'uméro',
+  'o ': 'uméro',
+  'os ': 'uméros',
+}
+
+if not os.path.isdir(args.output):
+  print('Directory does not exists', args.output, file=sys.stderr)
+  sys.exit(1)
 
 for event, node in doc:
   if not is_syceron:
@@ -57,8 +70,11 @@ for event, node in doc:
       visited.append(node.tagName)
       
       if node.tagName == "DateSeance":
-        if not args.print_tree:
-          print(seance_context)
+        if seance_context is not None:
+          output_seance_name = os.path.join(args.output, seance_context['DateSeance'][0]) + '.txt'
+          print('output_seance_name', output_seance_name)
+          with open(output_seance_name, 'w') as output_seance:
+            output_seance.write(json.dumps(seance_context))
 
         seance_context = {}
 
@@ -77,6 +93,26 @@ for event, node in doc:
         seance_context[visited[-1:][0]].append(node.nodeValue)
       except KeyError:
         seance_context[visited[-1:][0]] = [ node.nodeValue ]
+    else:
+      ## Collasping childrens of "texte" such as "exposant", "italique", ...
+      if len(visited) >= 2 and visited[-2] == 'texte':
+        ##print("LOLILOL", visited[-2], visited[-1], seance_context[visited[-2:][0]])
+        toAdd = node.nodeValue
+
+        if visited[-1] == 'indice':
+          print(visited[-1], "'{}'".format(node.nodeValue), seance_context[visited[-2:][0]][-1])
+
+        if visited[-1] == 'exposant':
+          if node.nodeValue in chars_mapping:
+            toAdd = chars_mapping[node.nodeValue]
+          else:
+            print(visited[-1], "'{}'".format(node.nodeValue), seance_context[visited[-2:][0]][-1])
+
+        try:
+          seance_context[visited[-2:][0]][-1] += toAdd
+        except KeyError:
+          print("KeyError", visited, toAdd)
+          ##seance_context[visited[-2:][0]][-1] = [ node.nodeValue ]
 
   if event == END_ELEMENT:
     indent_level -= 2
