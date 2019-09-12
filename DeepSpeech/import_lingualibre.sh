f16ff343842b5dc7dd413cb850afa6d61b9e928f
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-09-12 14:22:56 +0200

    Download italian file from lingualibre

--- DeepSpeech/import_lingualibre.sh
+++ DeepSpeech/import_lingualibre.sh
@@ -7,11 +7,11 @@ pushd $HOME/ds/
 		IMPORT_AS_ENGLISH="--normalize"
 	fi;
 
-	if [ ! -f "/mnt/extracted/data/lingualibre/lingua_libre_Q21-fra-French_train.csv" ]; then
+	if [ ! -f "/mnt/extracted/data/lingualibre/lingua_libre_Q385-ita-Italian_train.csv" ]; then
 		python bin/import_lingua_libre.py                       \
-			--qId 21                                        \
-			--iso639-3 fra                                  \
-			--english-name French                           \
+			--qId 385                                        \
+			--iso639-3 ita                                  \
+			--english-name Italian                           \
 			${IMPORT_AS_ENGLISH}                            \
 			--bogus-records $HOME/lingua_libre_skiplist.txt \
 			/mnt/extracted/data/lingualibre
