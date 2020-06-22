9148113d4d8b68eb5c7576d97b3e45aeabbef563
Alexandre <alexandre@serveur>
2020-06-22 12:26:46 +0200

    Fix #117: Use in-tree LM generation

--- DeepSpeech/build_lm.sh
+++ DeepSpeech/build_lm.sh
@@ -2,53 +2,46 @@
 
 set -xe
 
-pushd /mnt/extracted
-	if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
-		OLD_LANG=${LANG}
-		export LANG=${LM_ICONV_LOCALE}
-	fi;
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	OLD_LANG=${LANG}
+	export LANG=${LM_ICONV_LOCALE}
+fi;
 
+pushd /mnt/extracted/
 	$HOMEDIR/${MODEL_LANGUAGE}/prepare_lm.sh
 
 	if [ ! -f "sources_lm.txt" ]; then
 		echo "Your language's prepare_lm.sh did not produce a sources_lm.txt file. Please fix."
 		exit 1
 	fi;
+popd
+
+pushd $HOME/ds/
 
 	if [ ! -f "/mnt/lm/kenlm.scorer" ]; then
 
-		python $HOME/counter.py sources_lm.txt top_words.txt 500000
+		python data/lm/generate_lm.py \
+			--input_txt /mnt/extracted/sources_lm.txt \
+			--output_dir /mnt/lm/ \
+			--top_k ${LM_TOP_K} \
+			--kenlm_bins $HOME/kenlm/build/bin/ \
+			--arpa_order 4 \
+			--max_arpa_memory "85%" \
+			--arpa_prune "0|0|1" \
+			--binary_a_bits 255 \
+			--binary_q_bits 8 \
+			--binary_type trie
 
-		lmplz	--order 4 \
-			--temp_prefix /mnt/tmp/ \
-			--memory 80% \
-			--text sources_lm.txt \
-			--arpa /mnt/lm/lm.arpa \
-			--skip_symbols \
-			--prune 0 0 1
-
-		filter single model:/mnt/lm/lm.arpa /mnt/lm/lm_filtered.arpa < top_words.txt
-
-		build_binary -a 255 \
-			-q 8 \
-			trie \
-			/mnt/lm/lm_filtered.arpa \
-			/mnt/lm/lm.binary
-
-		pushd $HOME/ds/
-			python data/lm/generate_package.py \
-				--alphabet /mnt/models/alphabet.txt \
-				--lm /mnt/lm/lm.binary \
-				--vocab /mnt/lm/lm_filtered.arpa \
-				--package /mnt/lm/kenlm.scorer \
-				--default_alpha ${LM_ALPHA} \
-				--default_beta ${LM_BETA}
-		popd
-
-		rm /mnt/lm/lm.arpa /mnt/lm/lm_filtered.arpa
-	fi;
-
-	if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
-		export LANG=${OLD_LANG}
+		python data/lm/generate_package.py \
+			--alphabet /mnt/models/alphabet.txt \
+			--lm /mnt/lm/lm.binary \
+			--vocab /mnt/lm/vocab-${LM_TOP_K}.txt \
+			--package /mnt/lm/kenlm.scorer \
+			--default_alpha ${LM_ALPHA} \
+			--default_beta ${LM_BETA}
 	fi;
 popd
+
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	export LANG=${OLD_LANG}
+fi;
