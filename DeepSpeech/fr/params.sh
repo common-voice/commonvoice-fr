1685120f0cb3a91df73d211b89148f634fc05551
Alexandre <alexandre@serveur>
2020-06-22 18:00:43 +0200

    Use proper path for skiplist / validate_label

--- DeepSpeech/fr/params.sh
+++ DeepSpeech/fr/params.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-export IMPORTERS_VALIDATE_LOCALE="--validate_label_locale ${MODEL_LANGUAGE}/validate_label.py"
+export IMPORTERS_VALIDATE_LOCALE="--validate_label_locale $HOME/${MODEL_LANGUAGE}/validate_label.py"
 
 export CV_RELEASE_FILENAME="cv-3-fr.tar.gz"
 export CV_RELEASE_SHA256="5ba6967d08aee255a36b2a8087cf638e499d163f"
@@ -10,7 +10,7 @@ export CV_RELEASE_SHA256="5ba6967d08aee255a36b2a8087cf638e499d163f"
 export LINGUA_LIBRE_QID="21"
 export LINGUA_LIBRE_ISO639="fra"
 export LINGUA_LIBRE_ENGLISH="French"
-export LINGUA_LIBRE_SKIPLIST="${MODEL_LANGUAGE}/lingua_libre_skiplist.txt"
+export LINGUA_LIBRE_SKIPLIST="$HOME/${MODEL_LANGUAGE}/lingua_libre_skiplist.txt"
 
 export M_AILABS_LANG="fr_FR"
 export M_AILABS_SKIP="monsieur_lecoq,les_mysteres_de_paris"
