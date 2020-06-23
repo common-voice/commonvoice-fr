32d3e0f02557b360f4f8181b457a2e728af6c716
Alexandre <alexandre@serveur>
2020-06-23 10:36:17 +0200

    Fix #105: Use LM optimizer feature

--- DeepSpeech/evaluate_lm.sh
+++ DeepSpeech/evaluate_lm.sh
@@ -10,11 +10,12 @@ pushd $HOME/ds/
 		exit 0
 	fi;
 
-	for lm_range in ${LM_EVALUATE_RANGE}; do
-		LM_ALPHA="$(echo ${lm_range} |cut -d',' -f1)"
-		LM_BETA="$(echo ${lm_range} |cut -d',' -f2)"
+	if [ ! -z "${LM_EVALUATE_RANGE}" ]; then
+		LM_ALPHA_MAX="$(echo ${LM_EVALUATE_RANGE} |cut -d',' -f1)"
+		LM_BETA_MAX="$(echo ${LM_EVALUATE_RANGE} |cut -d',' -f2)"
+		LM_N_TRIALS="$(echo ${LM_EVALUATE_RANGE} |cut -d',' -f3)"
 		
-		python -u evaluate.py \
+		python -u lm_optimizer.py \
 			--show_progressbar True \
 			--train_cudnn True \
 			--alphabet_config_path /mnt/models/alphabet.txt \
@@ -23,8 +24,9 @@ pushd $HOME/ds/
 			--test_files ${all_test_csv} \
 			--test_batch_size ${BATCH_SIZE} \
 			--n_hidden ${N_HIDDEN} \
-			--lm_alpha ${LM_ALPHA} \
-			--lm_beta ${LM_BETA} \
+			--lm_alpha_max ${LM_ALPHA_MAX} \
+			--lm_beta_max ${LM_BETA_MAX} \
+			--n_trials ${LM_N_TRIALS} \
 			--checkpoint_dir /mnt/checkpoints/
-	done;
+	fi;
 popd
