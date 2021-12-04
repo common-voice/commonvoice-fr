222f2e60648c84fe2a0bf8ee13e465cf206c224c
wasertech <danny@waser.tech>
2021-12-04 08:32:18 +0100

    Super user what? Never heard of it.

--- STT/checks.sh
+++ STT/checks.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-sudo id
+sudo id #Super user what? Never heard of it.
 
 # Workaround libnvidia-ml.so: https://github.com/NVIDIA/nvidia-docker/issues/854#issuecomment-451464721
-sudo /sbin/ldconfig
+sudo /sbin/ldconfig # pretty sure this is fix right?
 
 nvidia-smi
 
@@ -30,6 +30,7 @@ mkdir /mnt/extracted/data/ || true
 python -c "import tensorflow as tf; tf.test.is_gpu_available()"
 
 # Checking with basic LDC93S1 before running into heavy-load
-pushd $HOME/ds/
-	./bin/run-tc-ldc93s1_new.sh 2 16000
+pushd $HOME/stt/
+	./bin/run-ldc93s1.sh
+    rm -rf ~/.local/share/stt
 popd
