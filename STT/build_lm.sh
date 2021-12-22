8d926310144c9f61c40650a5a302ace7350729e6
wasertech <danny@waser.tech>
2021-12-22 16:09:30 +0100

    this is how i should have done it from the begining

--- STT/build_lm.sh
+++ STT/build_lm.sh
@@ -32,7 +32,7 @@ pushd $STT_DIR
 			--binary_type trie
 	fi;
 
-	${STT_DIR}/data/lm/generate_scorer_package \
+	${STT_DIR}/generate_scorer_package \
 		--alphabet /mnt/models/alphabet.txt \
 		--lm /mnt/lm/lm.binary \
 		--vocab /mnt/lm/vocab-${LM_TOP_K}.txt \
