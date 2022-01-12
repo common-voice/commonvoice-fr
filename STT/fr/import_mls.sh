af6c63504cf859fc4ae4ad8bd80b622157681ff1
wasertech <danny@waser.tech>
2022-01-12 23:02:26 +0100

    Added support for french to MLS

--- STT/fr/import_mls.sh
+++ STT/fr/import_mls.sh
@@ -7,8 +7,8 @@ if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
 	IMPORT_AS_ENGLISH="--normalize"
 fi;
 
-if [ ! -f "/mnt/extracted/data/MLS/train.csv" ]; then
-	python ${STT_DIR}/bin/import_slr94.py \
+if [ ! -f "/mnt/extracted/data/MLS/MLS_train.csv" ]; then
+	python ${STT_DIR}/bin/import_mls.py \
 		${IMPORT_AS_ENGLISH} \
 		${IMPORTERS_VALIDATE_LOCALE} \
 		-l french \
