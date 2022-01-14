3cd419a0710cb3f5efbef4d2e760c0ada9fe1166
wasertech <danny@waser.tech>
2022-01-14 18:45:38 +0100

    fi xmswc import path

--- STT/fr/import_mswc.sh
+++ STT/fr/import_mswc.sh
@@ -9,10 +9,10 @@ if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
 	IMPORT_AS_ENGLISH="--english-compatible"
 fi;
 
-if [ ! -f "/mnt/extracted/data/mswc/mscw/ts_2019-04-11_fr_FR_train.csv" ]; then
+if [ ! -f "/mnt/extracted/data/MSWC/mscw/ts_2019-04-11_fr_FR_train.csv" ]; then
 	python ${STT_DIR}/bin/import_mswc.py \
 		${IMPORT_AS_ENGLISH} \
 		${IMPORTERS_VALIDATE_LOCALE} \
-		/mnt/extracted/data/mswc
+		/mnt/extracted/data/MSWC
 fi;
 #popd
