a3a69124ebc8593fd405c2ec155ae34c1ed5e3d3
wasertech <danny@waser.tech>
2022-01-15 07:41:22 +0100

    Add support for ts

--- STT/fr/import_ts.sh
+++ STT/fr/import_ts.sh
@@ -9,10 +9,10 @@ if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
 	IMPORT_AS_ENGLISH="--english-compatible"
 fi;
 
-if [ ! -f "/mnt/extracted/data/MSWC/mscw/ts_2019-04-11_fr_FR_train.csv" ]; then
-	python ${STT_DIR}/bin/import_mswc.py \
+if [ ! -f "/mnt/extracted/data/TrainingSpeech/ts_2019-04-11_fr_FR_train.csv" ]; then
+	python ${STT_DIR}/bin/import_ts.py \
 		${IMPORT_AS_ENGLISH} \
 		${IMPORTERS_VALIDATE_LOCALE} \
-		/mnt/extracted/data/MSWC
+		/mnt/extracted/data/TrainingSpeech
 fi;
 #popd
--- STT/fr/importers.sh
+++ STT/fr/importers.sh
@@ -6,7 +6,7 @@ set -xe
 
 . ${HOME}/import_lingualibre.sh
 
-. ./import_mswc.sh
+. ./import_ts.sh
 
 . import_slr57.sh
 
