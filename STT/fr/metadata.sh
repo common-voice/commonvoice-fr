a0a3902c0f63601aa6f9657a9ce6e6f748776033
wasertech <danny@waser.tech>
2022-01-27 15:46:26 +0100

    bump to cv-8

--- STT/fr/metadata.sh
+++ STT/fr/metadata.sh
@@ -4,7 +4,7 @@ set -xe
 
 export METADATA_AUTHOR="STT-FR-Team"
 export METADATA_MODEL_NAME="STT-fr"
-export METADATA_MODEL_VERSION="1.1-cv-7"
+export METADATA_MODEL_VERSION="1.1-cv-8"
 export METADATA_CONTACT_INFO="https://discourse.mozilla.org/c/voice/fr"
 export METADATA_LICENSE="MIT-0"
 export METADATA_LANGUAGE="fr-FR"
