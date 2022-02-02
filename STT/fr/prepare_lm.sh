4182ad8076548ede5b10484ecfa97f240fd9f474
wasertech <danny@waser.tech>
2022-02-02 16:37:35 +0100

    update fr/prepare_lm.sh

--- STT/fr/prepare_lm.sh
+++ STT/fr/prepare_lm.sh
@@ -21,9 +21,18 @@ if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
 	> wiki_fr_lower_accents.txt
 fi;
 
+if [ ! -f "mls_lm_french.txt" ]; then
+
+	if [ ! -d "/mnt/extracted/data/MLS/mls_lm_french" ]; then
+		./import_mls.sh
+	fi;
+	
+	cat /mnt/extracted/data/MLS/mls_lm_french/data.txt | tr '[:upper:]' '[:lower:]' > mls_lm_french.txt
+fi;
+
 # Remove special-char <s> that will make KenLM tools choke:
 # kenlm/lm/builder/corpus_count.cc:179 in void lm::builder::{anonymous}::ComplainDisallowed(StringPiece, lm::WarningAction&) threw FormatLoadException.
 # Special word <s> is not allowed in the corpus.  I plan to support models containing <unk> in the future.  Pass --skip_symbols to convert these symbols to whitespace.
 if [ ! -f "sources_lm.txt" ]; then
-	cat wiki_fr_lower.txt debats-assemblee-nationale.txt | sed -e 's/<s>/ /g' > sources_lm.txt
+	cat wiki_fr_lower.txt debats-assemblee-nationale.txt mls_lm_french.txt | sed -e 's/<s>/ /g' > sources_lm.txt
 fi;
