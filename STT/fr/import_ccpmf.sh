86bcde0ca3eaea49e393b8b84bbab6834f99f027
wasertech <danny@waser.tech>
2022-02-02 16:55:05 +0100

    updated fr/import_ccpmf.sh

--- STT/fr/import_ccpmf.sh
+++ STT/fr/import_ccpmf.sh
@@ -2,13 +2,14 @@
 
 set -xe
 
-pushd $HOME/ds/
-	if [ ! -f "/mnt/extracted/data/ccpmf/transcriptionsXML_audioMP3_MEFR_CCPMF_2012-2020/ccpmf_train.csv" ]; then
-		#  Hot patching like that.
-		sed -ri 's/MAX_SECS = .*/MAX_SECS = 4.5/g' bin/import_ccpmf.py
+#pushd $STT_DIR
+if [ ! -f "/mnt/extracted/data/ccpmf/transcriptionsXML_audioMP3_MEFR_CCPMF_2012-2020/ccpmf_train.csv" ]; then
+	#  Hot patching like that.
+	sed -ri 's/MAX_SECS = .*/MAX_SECS = 4.5/g' ${STT_DIR}/bin/import_ccpmf.py
 
-		python bin/import_ccpmf.py \
-			${IMPORTERS_VALIDATE_LOCALE} \
-			/mnt/extracted/data/ccpmf/
-	fi;
-popd
+	python ${STT_DIR}/bin/import_ccpmf.py \
+		${IMPORTERS_VALIDATE_LOCALE} \
+		--normalize \
+		/mnt/extracted/data/ccpmf/
+fi;
+#popd
