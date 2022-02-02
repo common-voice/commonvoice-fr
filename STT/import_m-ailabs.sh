4d3abbfee467126b26a7637272c5b6b6c70666bf
wasertech <danny@waser.tech>
2022-02-02 16:28:50 +0100

    update import_m-ailabs.sh

--- STT/import_m-ailabs.sh
+++ STT/import_m-ailabs.sh
@@ -2,20 +2,20 @@
 
 set -xe
 
-pushd $HOME/ds/
-	if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
-		IMPORT_AS_ENGLISH="--normalize"
+#pushd $STT_DIR
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	IMPORT_AS_ENGLISH="--normalize"
+fi;
+
+if [ ! -f "/mnt/extracted/data/M-AILABS/${M_AILABS_LANG}/${M_AILABS_LANG}_train.csv" ]; then
+	if [ ! -z "${M_AILABS_SKIP}" ]; then
+		SKIPLIST="--skiplist ${M_AILABS_SKIP}"
 	fi;
 
-	if [ ! -f "/mnt/extracted/data/M-AILABS/${M_AILABS_LANG}/${M_AILABS_LANG}_train.csv" ]; then
-		if [ ! -z "${M_AILABS_SKIP}" ]; then
-			SKIPLIST="--skiplist ${M_AILABS_SKIP}"
-		fi;
-
-		python bin/import_m-ailabs.py ${IMPORT_AS_ENGLISH}      \
-			${SKIPLIST}                                     \
-			--language ${M_AILABS_LANG}                     \
-			${IMPORTERS_VALIDATE_LOCALE}                    \
-			/mnt/extracted/data/M-AILABS/
-	fi;
-popd
+	${VIRTUAL_ENV}/bin/python ${STT_DIR}/bin/import_m-ailabs.py ${IMPORT_AS_ENGLISH}      \
+		${SKIPLIST}                                     \
+		--language ${M_AILABS_LANG}                     \
+		${IMPORTERS_VALIDATE_LOCALE}                    \
+		/mnt/extracted/data/M-AILABS/
+fi;
+#popd
