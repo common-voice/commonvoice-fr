6dca6dd3c9ba15370580821e9bbdc50c1663d1d5
wasertech <danny@waser.tech>
2022-02-02 16:41:30 +0100

    update fr/importers.sh

--- STT/fr/importers.sh
+++ STT/fr/importers.sh
@@ -2,14 +2,18 @@
 
 set -xe
 
-../import_cv.sh
+. ${HOME}/import_cv.sh
 
-../import_lingualibre.sh
+. ${HOME}/import_lingualibre.sh
 
-import_trainingspeech.sh
+. ./import_ts.sh
 
-import_slr57.sh
+. import_slr57.sh
 
-../import_m-ailabs.sh
+. import_atthack.sh
 
-import_ccpmf.sh
+. import_mls.sh
+
+. ${HOME}/import_m-ailabs.sh
+
+#. ./import_ccpmf.sh
