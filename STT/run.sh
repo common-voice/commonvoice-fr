7aaf725b93ee0bc87906484adfaeb443c16820bc
wasertech <danny@waser.tech>
2022-02-02 15:42:41 +0100

    update run.sh

--- STT/run.sh
+++ STT/run.sh
@@ -3,7 +3,7 @@
 set -xe
 
 THIS=$(dirname "$0")
-export PATH=${THIS}:${THIS}/${MODEL_LANGUAGE}:$PATH
+export PATH="${THIS}:${THIS}/${MODEL_LANGUAGE}:${STT_DIR}/training/coqui_stt_training:$PATH"
 
 export TF_CUDNN_RESET_RND_GEN_STATE=1
 
@@ -15,16 +15,16 @@ export TMP=/mnt/tmp
 export TEMP=/mnt/tmp
 
 . params.sh
-. ${MODEL_LANGUAGE}/params.sh
+. ${HOMEDIR}/${MODEL_LANGUAGE}/params.sh
 
-if [ -x "${MODEL_LANGUAGE}/metadata.sh" ]; then
-	. ${MODEL_LANGUAGE}/metadata.sh
+if [ -x "${HOMEDIR}/${MODEL_LANGUAGE}/metadata.sh" ]; then
+	. ${HOMEDIR}/${MODEL_LANGUAGE}/metadata.sh
 else
 	echo "Please prepare metadata informations."
 	exit 1
 fi;
 
-cd ${MODEL_LANGUAGE} && importers.sh && cd ..
+cd ${HOMEDIR}/${MODEL_LANGUAGE} && importers.sh && cd ${HOMEDIR}
 
 generate_alphabet.sh
 
