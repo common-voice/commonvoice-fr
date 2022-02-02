09323e945dbacbab5a4e47c6967d2da4ba4d2ac0
wasertech <danny@waser.tech>
2022-02-02 16:46:05 +0100

    update fr/import_slr57.sh

--- STT/fr/import_slr57.sh
+++ STT/fr/import_slr57.sh
@@ -2,15 +2,15 @@
 
 set -xe
 
-pushd $HOME/ds/
-	if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
-		IMPORT_AS_ENGLISH="--normalize"
-	fi;
+#pushd $STT_DIR
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	IMPORT_AS_ENGLISH="--normalize"
+fi;
 
-	if [ ! -f "/mnt/extracted/data/African_Accented_French/African_Accented_French/African_Accented_French_train.csv" ]; then
-		python bin/import_slr57.py \
-			${IMPORT_AS_ENGLISH} \
-			${IMPORTERS_VALIDATE_LOCALE} \
-			/mnt/extracted/data/African_Accented_French/
-	fi;
-popd
+if [ ! -f "/mnt/extracted/data/African_Accented_French/African_Accented_French/African_Accented_French_train.csv" ]; then
+	python ${STT_DIR}/bin/import_slr57.py \
+		${IMPORT_AS_ENGLISH} \
+		${IMPORTERS_VALIDATE_LOCALE} \
+		/mnt/extracted/data/African_Accented_French/
+fi;
+#popd
