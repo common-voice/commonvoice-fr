151bac117e8f2fb404097bfef918fe3e511b6b6d
wasertech <danny@waser.tech>
2022-02-02 19:57:07 +0100

    step 13: mv fr/metadata.sh

--- STT/fr/metadata.sh
+++ STT/fr/metadata.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-export METADATA_AUTHOR="DeepSpeech-FR-Team"
-export METADATA_MODEL_NAME="deepspeech-fr"
-export METADATA_MODEL_VERSION="0.6"
+export METADATA_AUTHOR="STT-FR-Team"
+export METADATA_MODEL_NAME="STT-fr"
+export METADATA_MODEL_VERSION="0.7"
 export METADATA_CONTACT_INFO="https://discourse.mozilla.org/c/voice/fr"
 export METADATA_LICENSE="MIT-0"
 export METADATA_LANGUAGE="fr-FR"
 export METADATA_MIN_DS_VERSION="0.7"
-export METADATA_MAX_DS_VERSION="0.9"
+export METADATA_MAX_DS_VERSION="1.9"
 export METADATA_DESCRIPTION="A free and re-usable French model for DeepSpeech"
