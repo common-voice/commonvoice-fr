54451416e6e354a14f3114f7d83e7dfdfb8c4184
wasertech <danny@waser.tech>
2022-02-03 19:20:10 +0100

    fix stt and model versions

--- STT/fr/metadata.sh
+++ STT/fr/metadata.sh
@@ -4,10 +4,10 @@ set -xe
 
 export METADATA_AUTHOR="STT-FR-Team"
 export METADATA_MODEL_NAME="STT-fr"
-export METADATA_MODEL_VERSION="0.7"
+export METADATA_MODEL_VERSION="0.8"
 export METADATA_CONTACT_INFO="https://discourse.mozilla.org/c/voice/fr"
 export METADATA_LICENSE="MIT-0"
 export METADATA_LANGUAGE="fr-FR"
-export METADATA_MIN_DS_VERSION="0.7"
-export METADATA_MAX_DS_VERSION="1.9"
+export METADATA_MIN_STT_VERSION="0.7"
+export METADATA_MAX_STT_VERSION="1.9"
 export METADATA_DESCRIPTION="<A free and re-usable French model for STT>"
