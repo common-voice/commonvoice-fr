f794d071723fdf0373d8ccb575297786f243cbd2
wasertech <danny@waser.tech>
2022-05-12 02:53:48 +0200

    this will fail in so many ways

--- DeepSpeech/checks.sh
+++ DeepSpeech/checks.sh
@@ -31,5 +31,5 @@ python -c "import tensorflow as tf; tf.test.is_gpu_available()"
 
 # Checking with basic LDC93S1 before running into heavy-load
 pushd $HOME/ds/
-	./bin/run-tc-ldc93s1_new.sh 2 16000
+	./bin/run-ci-ldc93s1_new.sh 2 16000
 popd
