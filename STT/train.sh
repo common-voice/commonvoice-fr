483efe4d9f329f4a30258869ce0418c7f00c8464
wasertech <danny@waser.tech>
2022-05-29 00:50:59 +0200

    Add basic data augmentation

--- STT/train.sh
+++ STT/train.sh
@@ -34,6 +34,25 @@ pushd ${STT_DIR}
 		SKIP_BATCH_TEST_FLAG="--skip_batch_test true"
 	fi;
 
+	# Basic augmentation for data
+	# TODO: Add use of overlays with noise datasets
+	# ^ This would require to download and prepare noise data
+	ALL_AUGMENT_FLAGS=""
+	if [ "${ENABLE_AUGMENTS}" = "1" ]; then
+		ALL_AUGMENT_FLAGS='--cache_for_epochs 10'
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} --augment reverb[p=0.1,delay=50.0~30.0,decay=10.0:2.0~1.0]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} resample[p=0.1,rate=12000:8000~4000]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} codec[p=0.1,bitrate=48000:16000]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} volume[p=0.1,dbfs=-10:-40]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} pitch[p=0.1,pitch=1~0.2]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} tempo[p=0.1,factor=1~0.5]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} frequency_mask[p=0.1,n=1:3,size=1:5]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} time_mask[p=0.1,domain=signal,n=3:10~2,size=50:100~40]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} dropout[p=0.1,rate=0.05]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} add[p=0.1,domain=signal,stddev=0~0.5]"
+		ALL_AUGMENT_FLAGS="${ALL_AUGMENT_FLAGS} multiply[p=0.1,domain=features,stddev=0~0.5]"
+	fi;
+
 	# Check metadata existence
 	if [ -z "$METADATA_AUTHOR" ]; then
 		echo "Please fill-in metadata informations"
@@ -75,6 +94,7 @@ pushd ${STT_DIR}
 			--log_level=${LOG_LEVEL} \
 			${EARLY_STOP_FLAG} \
 			${LOAD_CHECKPOINT_FROM} \
-			${SKIP_BATCH_TEST_FLAG}
+			${SKIP_BATCH_TEST_FLAG} \
+			${ALL_AUGMENT_FLAGS}
 	fi;
 popd
